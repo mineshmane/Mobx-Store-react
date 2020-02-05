ad55cc8344ed51eda72cb32bbee10e4affbc4d7d
mineshmane <mineshmane94@gmail.com>
2020-02-05 12:33:55 +0530

    Mobx implementaaion with store

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Form from './components/Form';
+import Dashboard from './components/Dashboard';
+import Reviews from './components/Reviews';
+
+
+import Store from './Store';
+import {decorate, observable, action, computed} from 'mobx';
+
+decorate(Store, {
+  reviewList: observable,
+  addReview: action,
+  averageScore: computed,
+  reviewCount: computed
+});
+const reviewStore = new Store();
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+        <Form store={reviewStore}/>
+        <Dashboard store={reviewStore}/>
+        <Reviews store={reviewStore}/>
+      </div>
   );
 }
 
